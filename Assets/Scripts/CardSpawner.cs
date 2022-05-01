dbfc20037e700f98bf48a7afa995c66f0bb929c5
Vanna <vannaerr404@gmail.com>
2022-05-01 02:05:16 -0500

    bullshit

--- Assets/Scripts/CardSpawner.cs
+++ Assets/Scripts/CardSpawner.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class CardSpawner : MonoBehaviour
 {
     private int cardCount = 0;
-    public GameObject[] cardPositions;
+    public float arc;
+    public GameObject cardPosition;
     public ScriptableObject[] cards;
     public GameObject cardPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(cards.Length);
-    }
+    public float maxAngle;
+    private List<GameObject> cardList = new List<GameObject>();
     public void SpawnCard()
     {
+        
+        
+        float angle = Mathf.Clamp(arc/(cardCount-1), 0, maxAngle);
         int rand = Random.Range(0, cards.Length);
         Debug.Log(rand);
-        GameObject newCard = Instantiate(cardPrefab, cardPositions[cardCount].transform.position, Quaternion.identity, FindObjectOfType<Canvas>().transform);
+        GameObject newCard = Instantiate(cardPrefab, cardPosition.transform.position, Quaternion.identity, cardPosition.transform);
         cardCount++;
         newCard.GetComponent<CardDisplay>().card = cards[rand] as TowerCard;
+        cardList.Add(newCard);
+        bool isEven = cardCount.IsEven();
+        angle = isEven? angle/2f : angle;
+        for (int i = 0; i < cardCount; i++){
+            if (isEven)
+            {
+                
+            }
+            else
+            {
+                float theta = (((((float)cardCount/2f).Floor() * angle)*-1)+i*angle)+90;
+                Debug.Log(theta);
+                theta = theta * Mathf.Deg2Rad;
+                Vector2 potato = new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
+                cardList[i].transform.localPosition = potato * 900;
+                cardList[i].transform.up = potato;
+                 
+            }
+        }
+
+        
+
     }
 }
