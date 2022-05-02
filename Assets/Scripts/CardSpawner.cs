ce19e297274e35820ef0c13dbc01300633880d52
Vanna <vannaerr404@gmail.com>
2022-05-01 23:25:30 -0500

    Drawing Cards

--- Assets/Scripts/CardSpawner.cs
+++ Assets/Scripts/CardSpawner.cs
@@ -11,37 +11,29 @@ public class CardSpawner : MonoBehaviour
     public GameObject cardPrefab;
     public float maxAngle;
     private List<GameObject> cardList = new List<GameObject>();
+    private float angle;
+
     public void SpawnCard()
     {
-        
-        
-        float angle = Mathf.Clamp(arc/(cardCount-1), 0, maxAngle);
+        angle = Mathf.Clamp(arc/(cardCount-1), 0, maxAngle);
         int rand = Random.Range(0, cards.Length);
-        Debug.Log(rand);
         GameObject newCard = Instantiate(cardPrefab, cardPosition.transform.position, Quaternion.identity, cardPosition.transform);
         cardCount++;
         newCard.GetComponent<CardDisplay>().card = cards[rand] as TowerCard;
         cardList.Add(newCard);
+        SortCards();
+    }
+    public void SortCards()
+    {
         bool isEven = cardCount.IsEven();
-        angle = isEven? angle/2f : angle;
-        for (int i = 0; i < cardCount; i++){
-            if (isEven)
-            {
-                
-            }
-            else
-            {
-                float theta = (((((float)cardCount/2f).Floor() * angle)*-1)+i*angle)+90;
-                Debug.Log(theta);
+        for (int i = 0; i < cardCount; i++)
+        {
+                float theta = ((((((float)cardCount/2f).Floor() * angle)*-1)+i*angle)+90);
+                if(isEven) theta = theta + (angle/2f);
                 theta = theta * Mathf.Deg2Rad;
                 Vector2 potato = new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
                 cardList[i].transform.localPosition = potato * 900;
                 cardList[i].transform.up = potato;
-                 
-            }
         }
-
-        
-
     }
 }
---
+++ Assets/Scripts/CardZoom.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CardZoom : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private Transform cardZoomPoint;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
