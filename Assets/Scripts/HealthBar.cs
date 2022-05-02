a4ed744749f7f8505637f13e5ab33a8eed35072a
Avea <aveakrause@gmail.com>
2022-05-01 23:59:46 -0700

    health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private SlicedFilledImage fill;
+    [SerializeField] private TextMeshProUGUI text;
+    public IHealth health {get; private set;}
+    public Gradient gradient;
+
+    public void Track(IHealth health) {
+        this.health = health;
+        health.onHealthChanged += OnHealthChanged;
+        OnHealthChanged(health.currentHP / health.maxHP);
+    }
+    private void OnDestroy() => health.onHealthChanged -= OnHealthChanged;
+
+    private void OnHealthChanged(float percent) 
+    {
+        fill.color = gradient.Evaluate(percent);
+        text.text = $"{health.currentHP}";
+        fill.fillAmount = percent;  
+    }
+}
