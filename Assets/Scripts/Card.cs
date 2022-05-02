7cc06426e5c359427ca7d41b765c3acdffdedb5c
Vanna <vannaerr404@gmail.com>
2022-05-02 01:49:17 -0500

    Card zoom on mouse over

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Card : MonoBehaviour
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class Card : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    private GameObject zoomedCard;
+    private GameObject parent;
+    private GameObject cardPosition;
+    public float offset;
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        zoomedCard = Instantiate(parent, Vector3.zero, Quaternion.identity, cardPosition.transform);
+        zoomedCard.transform.localPosition = new Vector3(parent.transform.localPosition.x, zoomedCard.transform.localPosition.y+offset, parent.transform.localPosition.z);
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Destroy(zoomedCard);
+    }
+
+    void Start() 
+    {
+        //ugly hack to get the parent of the card
+        cardPosition = gameObject.transform.parent.gameObject.transform.parent.gameObject;
+        parent = gameObject.transform.parent.gameObject;
+    }
 }
