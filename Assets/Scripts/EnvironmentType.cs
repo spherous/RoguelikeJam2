7317131550947199ed5abe5a8494ac08f6b598ec
Avea <aveakrause@gmail.com>
2022-05-12 17:17:47 -0700

    WIP
    build/remove buildable tiles

--- Assets/Scripts/EnvironmentType.cs
+++ Assets/Scripts/EnvironmentType.cs
@@ -1,12 +1,14 @@
 using System;
 
 public enum EnvironmentType {
-    None = 0, 
+    None = 0,
+    CreateBuildableTile = 1,
+    RemoveBuildableTile = 2,
     SlowMod = 8,
     StunMod = 9,
     ThornsMod = 10,
     WoundingMod = 11,
-    DamageAmpMod = 12
+    DamageAmpMod = 12,
 }
 
 public static class EnvironmentTypeExtensions
@@ -25,7 +27,23 @@ public static class EnvironmentTypeExtensions
         EnvironmentType.DamageAmpMod => () => GrantWebEffect(tower, WebEffect.DamageAmp),
         _ => null
     };
-    
+
+    public static Action PlayOnTile(this EnvironmentType type, Tile tile) => type switch{
+        EnvironmentType.CreateBuildableTile => () => CreateBuildable(tile),
+        EnvironmentType.RemoveBuildableTile => () => RemoveBuildable(tile),
+        _ => null
+    };
+
+    private static void RemoveBuildable(Tile tile)
+    {
+        
+    }
+
+    private static void CreateBuildable(Tile tile)
+    {
+        
+    }
+
     public static void GrantWebEffect(ITower tower, WebEffect webEffect)
     {
         if(tower is WebTower webTower)
